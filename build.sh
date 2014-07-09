3fc7c0c8d04bbd7e3724d93c15387e5291b9cd7c
Megax <megax@yeahunter.hu>
2014-07-09 17:24:23 +0200

    * build script hozzáadva.

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+
+build_conf="$(echo "$1" | tr [:upper:] [:lower:])"
+
+if [ "$2" = "" ]; then
+	build_platform=""
+else
+	build_platform="$(echo "$2" | tr [:upper:] [:lower:])"
+fi
+
+if [ "$1" = "" ]; then
+	xbuild /p:Configuration="Release" SGit.sln /flp:LogFile=xbuild.log;Verbosity=Detailed
+else
+	if [ "$build_platform" = "" ]; then
+		if [ $build_conf = "debug" ]; then
+			xbuild /p:Configuration="Debug" SGit.sln /flp:LogFile=xbuild.log;Verbosity=Detailed
+		else
+			xbuild /p:Configuration="Release" SGit.sln /flp:LogFile=xbuild.log;Verbosity=Detailed
+		fi
+	else
+		if [ $build_conf = "debug" ]; then
+			xbuild /p:Configuration="Debug" /p:PlatformTarget=$build_platform SGit.sln /flp:LogFile=xbuild.log;Verbosity=Detailed
+		else
+			xbuild /p:Configuration="Release" /p:PlatformTarget=$build_platform SGit.sln /flp:LogFile=xbuild.log;Verbosity=Detailed
+		fi
+	fi
+fi
